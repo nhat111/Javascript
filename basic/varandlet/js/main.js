224c8a8bfca519a4fe14106e82cb0ac906aa8a01
Nguyễn Nhật <nhat10t3@gmail.com>
2020-06-12 06:25:22 +0700

    add object

---
+++ basic/varandlet/js/main.js
@@ -0,0 +1,9 @@
+    var greeting = "hey hi";
+    var times = 4;
+
+    if (times > 3) {
+        var greeting = "say Hello instead"; 
+        // 
+    }
+
+    console.log(greeting); //"say Hello instead"
